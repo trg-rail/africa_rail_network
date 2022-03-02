1515b17c786083d78f9d112552616ec9bc831dfe
Marcus Young <m.a.young@soton.ac.uk>
2022-03-02 22:51:45 +0000

    Morocco initial routable network complete

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # africa_rail_network
+
+Progress:
+
+- [x] Prepare database tables
+- [ ] Morocco
+  - [x] Open and disused lines - passenger and freight - including line names and gauge
+  - [x] Stations, ports and freight destinations
+  - [ ] Tram/light rail (defer)
+  - [ ] Identify closed stations (defer)
+  - [ ] Speed cost (defer)
+  - [ ] Abandoned lines (defer)
+  - [ ] Proposed lines (defer)
+
+
+
--- morocco/morocco.sql
+++ morocco/morocco.sql
@@ -19,6 +19,16 @@ set name = 'Marrakech مراكش',
 railway = 'station'
 where oid = 555134634;
 
+update africa_osm_nodes
+set name = 'Gare d''El Jadida',
+railway = 'station'
+where oid = 555074025;
+
+update africa_osm_nodes
+set name = 'Gare de Sidi Bouknadel',
+railway = 'station'
+where oid = 555062472;
+
 
 -- Taourirt - current railway value platform
 update africa_osm_nodes
@@ -30,7 +40,135 @@ set name = 'Bouarfa',
 railway = 'station'
 where oid = 555107745;
 
+-- add latin name where just arabic (if available)
+
+update africa_osm_nodes
+set name = 'Nzalat Laadam نزالة العظم',
+railway = 'station'
+where oid = 555027384;
+
+update africa_osm_nodes
+set name = 'Sidi Bou Othmane بوناكة',
+railway = 'station'
+where oid = 555007958;
+
+
+
+-- set names where missing
+update africa_osm_nodes
+set name = 'Skhour Rhamna',
+railway = 'station'
+where oid = 555027383;
+
+update africa_osm_nodes
+set name = 'Sidi Abdallah',
+railway = 'station'
+where oid = 555007962;
+
+update africa_osm_nodes
+set name = 'unnamed',
+railway = 'station'
+where oid in (555037821, 555021325, 555022954, 555013399, 555025695);
+
+update africa_osm_nodes
+set name = 'Souaken',
+railway = 'station'
+where oid = 555025149;
+
+update africa_osm_nodes
+set name = 'Bab Tissera',
+railway = 'station'
+where oid = 555021322;
+
+update africa_osm_nodes
+set name = 'Ain Karma',
+railway = 'station'
+where oid = 555033248;
+
+update africa_osm_nodes
+set name = 'Fes Ville',
+railway = 'station'
+where oid = 555035429;
+
+update africa_osm_nodes
+set name = 'Touabaa',
+railway = 'station'
+where oid = 555025802;
+
+update africa_osm_nodes
+set name = 'Oued Amlil'
+where oid = 555046416;
+
+update africa_osm_nodes
+set name = 'Nouaceur',
+railway = 'station'
+where oid = 556031481;
+
+
 -- facilities
+
+-- mines / industrial etc
+
+-- OCP Phosphate mine, Benguerir
+update africa_osm_nodes
+set railway = 'stop',
+name = 'OCP Benguerir (Phosphate Mine)',
+facility = 'mine'
+where oid = 555076761;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'OCP Youssoufia (Phosphate Mine)',
+facility = 'mine'
+where oid = 555132339;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'OCP Khouribga (Phosphate Mine)',
+facility = 'mine'
+where oid = 555077215;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'OCP Safi (Phosphate processing plant)',
+facility = 'chemical_plant'
+where oid = 555071248;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Ben Guerir Air Base',
+facility = 'military'
+where oid = 555063407;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Renault-Nissan Group',
+facility = 'manufacturing'
+where oid = 555089774;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'LafargeHolcim Maroc cement works (Fes)',
+facility = 'manufacturing'
+where oid = 555079035;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'LafargeHolcim Maroc cement works (Meknes)',
+facility = 'manufacturing'
+where oid = 555117768;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Sonasid steel works',
+facility = 'manufacturing'
+where oid = 555090096;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Jerada Power Station',
+facility = 'utilities'
+where oid = 555100350;
 
 -- ports
 update africa_osm_nodes
@@ -41,25 +179,52 @@ where oid = 555083287;
 
 update africa_osm_nodes
 set railway = 'stop',
+name = 'Port of Jorf Lasfar',
+facility = 'port'
+where oid = 555118287;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Port of Safi',
+facility = 'port'
+where oid = 555101363;
+
+update africa_osm_nodes
+set railway = 'stop',
 name = 'Beni Nsar (Nadar) Port',
 facility = 'port'
 where oid = 555121673;
+
+update africa_osm_nodes
+set railway = 'stop',
+name = 'Port of Casablanca',
+facility = 'port'
+where oid = 555118206;
+
 
 
 -- create new station nodes
 -- this is required as there can be several edges running through stations but the station node
 -- is located on an edge that isn't used for the route.
 -- Taourirt 
-
+-- Chtouka
+-- Nouaceur
+-- Khouribga
+-- Mers Sultan
+-- Casa Voyageurs
+-- Ain-Sebaa
+-- Tangier Morora
 
 DO $$ DECLARE
 -- create new station nodes
 -- note: must not be a node coincident with the closest point (reassign that node as a station instead)
-nodes INT8 ARRAY DEFAULT ARRAY [ ];
-edges INT8 ARRAY DEFAULT ARRAY [ ];
+--nodes INT8 ARRAY DEFAULT ARRAY [555028771, 8283175981, 555031481, 555024932, 555007318, 555048489, 555062408, 555062153];
+--edges INT8 ARRAY DEFAULT ARRAY [555084158, 555089696, 555004426, 555084551, 555004329, 555103683, 555004392, 555084807];
+nodes INT8 ARRAY DEFAULT ARRAY [555062153];
+edges INT8 ARRAY DEFAULT ARRAY [555084807];
 node INT8;
 edge INT8;
-idx INT8;
+idx INT;
 closest_point GEOMETRY;
 newline GEOMETRY;
 orsource int8;
@@ -84,22 +249,24 @@ select source from africa_osm_edges where oid = edge into orsource;
 raise notice 'counter: %', node || ' ' || edge || ' ' || idx || ' ' || orsource || ' ' || ortarget ;	
 
 -- create new node for station
-INSERT INTO africa_osm_nodes (name, geom, railway, oid)
-SELECT name, closest_point, railway, oid + 10000
+INSERT INTO africa_osm_nodes (name, geom, railway, country, oid)
+SELECT name, closest_point, railway, country, oid + 1000000
 FROM africa_osm_nodes
 WHERE oid = node;
  
 insert into africa_osm_edges 
 with tmp as ( select a.*, ( st_dump ( st_split ( newline, closest_point ) ) ).geom as geom2 from africa_osm_edges a where oid = edge ),
-	tmp2 as ( select geom2 as geom, length, ( oid :: text || row_number ( ) over ( ) ) :: int as oid, line, gauge, status, mode, structure, speed_freight, speed_passenger, comment, st_startpoint ( geom2 ), st_endpoint ( geom2 ) from tmp ) select
+	tmp2 as ( select geom2 as geom, country, length, ( oid :: text || row_number ( ) over ( ) ) :: int8 as oid, line, gauge, status, type, mode, structure, speed_freight, speed_passenger, comment, st_startpoint ( geom2 ), st_endpoint ( geom2 ) from tmp ) select
 	a.geom,
-	round( st_length ( st_transform ( a.geom, 32736 ) ) :: numeric, 2 ) as length,
+	a.country,
+	round(st_lengthspheroid(a.geom, 'SPHEROID["WGS 84",6378137,298.257223563]')::numeric,2 ) as length,
 	b.oid as source,
 	c.oid as target,
 	a.oid,
 	a.line,
 	a.gauge,
 	a.status,
+	a.type,
 	a.mode,
 	a.structure,
 	a.speed_freight,
@@ -133,8 +300,6 @@ with tmp as ( select a.*, ( st_dump ( st_split ( newline, closest_point ) ) ).ge
 END LOOP;
 END $$;
 
-
-
 -- psql code to fix routing issue. Splits edge at node.
 -- allow routing out of Tanger Ville for LGV
 -- split 88425 @ 77371
@@ -149,13 +314,32 @@ END $$;
 -- split 555034342 @ 555120820
 -- route from Rabat Agdal towards Temara
 -- split 555030256 @ 555128402
+-- routing into Jorf Lasfar port
+-- split 555029415 @ 555118290
+-- split 555013654 @ 555078433
+-- routing freight line from OCP Mine at Benguerir to Safi Port
+-- split 555023792 @ 555076760
+-- routing freight line Khouribga
+-- split 555084552 @ 555077214
+-- routing freight at Port of Casa
+-- split 555084178 @ 555118167
+-- routing to Renault-Nissan Factory
+-- split 555032589 @ 555089775
+-- LafargeHolcim Maroc cement works (Fes)
+-- split 555084194 @ 555117922
+-- split 555084138 @ 555079034
+-- LafargeHolcim Maroc cement works (Meknes)
+-- split 555029717 @ 555115192
+-- Jerada branch
+-- split 555030005 @ 555100535 
+-- split 5550300052 @ 555078983
 
 
 DO $$ DECLARE
--- edges INT8 ARRAY DEFAULT ARRAY [555088425, 5550884251, 555032587, 555091026, 5550910261, 555084141, 555034342, 555030256];
- -- nodes INT8 ARRAY DEFAULT ARRAY [555077371, 555074354, 555101620, 555123363, 555101619, 555117940, 555120820, 555128402];
-edges INT8 ARRAY DEFAULT ARRAY [555030256];
-nodes INT8 ARRAY DEFAULT ARRAY [555128402];
+-- edges INT8 ARRAY DEFAULT ARRAY [555088425, 5550884251, 555032587, 555091026, 5550910261, 555084141, 555034342, 555030256, 555029415, 555013654, 555023792, 555084552, 555084178, 555032589, 555084194, 555084138, 555029717, 555030005, 5550300052];
+ -- nodes INT8 ARRAY DEFAULT ARRAY [555077371, 555074354, 555101620, 555123363, 555101619, 555117940, 555120820, 555128402, 555118290, 555078433, 555076760, 555077214, 555118167, 555089775, 555117922, 555079034, 555115192, 555100535, 555078983];
+edges INT8 ARRAY DEFAULT ARRAY [5550300052];
+nodes INT8 ARRAY DEFAULT ARRAY [555078983];
 edge int8;
 node int8;
 BEGIN
@@ -541,7 +725,338 @@ update africa_osm_edges
 set line = 'Bouskoura to Mohammed V International Airport',
 gauge = '1435',
 status = 'open',
+comment = 'Part fo TNR Al Bidaoui (Casablanca - Mohammed V International Airport',
+mode = 'mixed'
+where oid in (select edge from tmp);
+
+-- Bouskoura to El Jadida
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+         555081547,
+		555074025,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Bouskoura to El Jadida',
+gauge = '1435',
+status = 'open',
+comment = 'Part of TNR Casablanca - El Jadida route',
+mode = 'mixed'
+where oid in (select edge from tmp);
+
+
+-- El Jadida to Port of Jorf Lasfar
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+         555074025,
+		555118287,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'El Jadida to Port of Jorf Lasfar',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+
+-- Need to edit edge 555032067 and amend the first coordinate (source) to be
+-- node 556028771 rather than 555028771 to simplify routing to Oued Zem.
+
+UPDATE africa_osm_edges
+	SET geom = ST_SetPoint(geom, 0, (select geom from africa_osm_nodes where oid = 556028771)),
+	source = 556028771
+	WHERE oid = 555032067;
+
+-- Sidi el Aidi to Oued Zem
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        556028771,
+		555028772,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Sidi el Aidi to Oued Zem',
+gauge = '1435',
+status = 'open',
 comment = '',
 mode = 'mixed'
 where oid in (select edge from tmp);
 
+-- Need to edit edge 555023792 and amend the last coordinate (target) to be
+-- node 555020393 rather than 555074682 to simplify routing to Safi branch line.
+
+UPDATE africa_osm_edges
+	SET geom = ST_SetPoint(geom, ST_NumPoints(geom) - 1, (select geom from africa_osm_nodes where oid = 555020393)),
+	target = 555020393
+	WHERE oid = 555023792;
+
+-- Ben Guerir to Safi
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555020393,
+		555028588,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Ben Guerir to Safi',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'mixed'
+where oid in (select edge from tmp);
+
+-- Safi to Port of Safi
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555028588,
+		555101363,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Safi to Port of Safi',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+
+-- OCP Mine (Benguerir) to Safi line
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555076761,
+		555076760,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'OCP Mine (Benguerir) to Safi line',
+gauge = '1435',
+status = 'open',
+comment = 'OCP Phosphate mines. Freight route to Safi Port',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- OCP Phosphate Processing plant
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555077822,
+		555071248,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'OCP phosphate processing plant',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- Benguerir Air Base
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555063406,
+		555063407,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Benguerir Air Base',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- OCP Youssoufia Mine
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+        555101617,
+		555132339,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'OCP Youssoufia Mine',
+gauge = '1435',
+status = 'open',
+comment = 'OCP Phosphate mines',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- OCP Khouribga Mine
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555077214,
+		555077215,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'OCP Khouribga Mine',
+gauge = '1435',
+status = 'open',
+comment = 'OCP Phosphate mines',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- Freight line to Port of Casa
+-- link to simplify routing
+-- 555118161 to 555118159
+with tmp as
+(
+select st_makeline(a.geom, b.geom) as line, a.country from africa_osm_nodes a, africa_osm_nodes b where a.oid = 555118161 and b.oid = 555118159
+)
+insert into africa_osm_edges
+select 
+a.line,
+a.country,
+round(st_lengthspheroid(a.line, 'SPHEROID["WGS 84",6378137,298.257223563]')::numeric,2) as length,
+555118161,
+555118159,
+556000005
+from tmp as a;
+
+-- Port of Casablanca (Freight)
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555118161,
+		555118206,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Port of Casablanca',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- Renault-Nissan Group (Freight)
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555089775,
+		555089774,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Renault-Nissan Group Factory',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- LafargeHolcim Maroc cement works (Fes)
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555081212,
+		555079035,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'LafargeHolcim Maroc cement works (Fes)',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- LafargeHolcim Maroc cement works (Meknes)
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555115192,
+		555117768,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'LafargeHolcim Maroc cement works (Meknes)',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+-- link 555090095 to 555020067
+with tmp as
+(
+select st_makeline(a.geom, b.geom) as line, a.country from africa_osm_nodes a, africa_osm_nodes b where a.oid = 555090095 and b.oid = 555020067
+)
+insert into africa_osm_edges
+select 
+a.line,
+a.country,
+round(st_lengthspheroid(a.line, 'SPHEROID["WGS 84",6378137,298.257223563]')::numeric,2) as length,
+555090095,
+555020067,
+556000006
+from tmp as a;
+
+
+-- Sonasid steel works (Nador)
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555020067,
+		555090096,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Sonasid steel works (Nador)',
+gauge = '1435',
+status = 'open',
+comment = '',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+
+-- Guenfouda to Jerada branch
+with tmp as(
+SELECT X.* FROM pgr_dijkstra(
+                'SELECT oid as id, source, target, length AS cost FROM africa_osm_edges',
+       555100535,
+		555100350,
+		false
+		) AS X
+		ORDER BY seq)
+update africa_osm_edges
+set line = 'Guenfouda to Jerada branch (Jerada Power Station)',
+gauge = '1435',
+status = 'open',
+comment = 'Appears to serve Jerada coal-powered power station. Coal imported via Nador port',
+mode = 'freight'
+where oid in (select edge from tmp);
+
+
+
+update africa_osm_nodes
+set gauge = '1435'
+where st_intersects(geom, (select st_collect(geom) from africa_osm_edges where gauge = '1435'))
+and railway in ('station', 'halt', 'stop');
